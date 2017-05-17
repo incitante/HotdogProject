cacceeb97c88303e69de0ca4bd89ad12b3b09c30
incitante <jiwan@cau.ac.kr>
2017-05-17 20:08:02 +0900

    Add files via upload

--- src/hotdogProject/compare.java
+++ src/hotdogProject/compare.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 
 public class compare {
-	ArrayList<String> left,right;
+	
 	private ArrayList<String> inputText(ArrayList<String> List){
 		
 		
@@ -15,10 +15,10 @@ public class compare {
 	private void setParagraph(Paragraph para,String text){
 		//System.out.print(text.getText());
 	}
-	public void compareCode(){
-		int k, i, j, t;
+	public static void compareCode(){
+		int k, i, j, t, limitK, limitI, limitJ;
 		int sw;
-		
+		ArrayList<String> left,right;
 		String text;
 		
 		left = new ArrayList<String>();
@@ -35,14 +35,32 @@ public class compare {
             // nextToken 인자에는 분리할 토큰(분리할 스트링을 넣어줍니다)  
             right.add(inputRight.nextToken("\n"));  
         }  
-				
-		k = i = j = t = 0;
+		
+		for(int a=0;a<left.size();a++) {
+			System.out.println(left.get(a));
+		}
+		
+		lcs.findLcs(left, right);
+		
+		k = i = j = 0;
+		t = -1;
 		sw = -1;
 		
-		while(true) {
+		limitK = lcs.getLcsSize();
+		limitI = lcs.getSizeOfLeftLCS();
+		limitJ = lcs.getSizeOfRightLCS();
+		
+		System.out.println(limitK);
+		System.out.println(limitI);
+		System.out.println(limitJ);
+		
+		while(k < limitK) {
 			if(lcs.getLeftLCS(k) == i && lcs.getRightLCS(k) == j) {
 				if(sw != 0) {
+					sw = 0;
+					t++;
 					ParagraphList.leftParagraphList.add(new Paragraph());
+					ParagraphList.rightParagraphList.add(new Paragraph());
 				}
 				ParagraphList.leftParagraphList.get(t).setSeq(left.get(lcs.getLeftLCS(i)));
 				ParagraphList.rightParagraphList.get(t).setSeq(right.get(lcs.getRightLCS(i)));
@@ -53,7 +71,9 @@ public class compare {
 			else if(lcs.getLeftLCS(k) == i && lcs.getRightLCS(k) != j) {
 				if(sw != 1) {
 					sw = 1;
+					t++;
 					ParagraphList.leftParagraphList.add(new Paragraph());
+					ParagraphList.rightParagraphList.add(new Paragraph());
 				}
 				ParagraphList.leftParagraphList.get(t).setSeq("\n");
 				ParagraphList.rightParagraphList.get(t).setSeq(right.get(lcs.getRightLCS(i)));
@@ -62,11 +82,40 @@ public class compare {
 			else {
 				if(sw != 2) {
 					sw = 2;
+					t++;
 					ParagraphList.leftParagraphList.add(new Paragraph());
+					ParagraphList.rightParagraphList.add(new Paragraph());
 				}
 				ParagraphList.leftParagraphList.get(t).setSeq(left.get(lcs.getLeftLCS(i)));
 				ParagraphList.rightParagraphList.get(t).setSeq("\n");
 				i++;
+			}
+		}
+		
+		if(i < limitI) {
+			ParagraphList.leftParagraphList.add(new Paragraph());
+			ParagraphList.rightParagraphList.add(new Paragraph());
+			t++;
+			while(i < limitI) {
+				ParagraphList.leftParagraphList.get(t).setSeq(left.get(lcs.getLeftLCS(i)));
+				ParagraphList.rightParagraphList.get(t).setSeq("\n");
+				i++;
+			}
+		}
+		
+		if(j < limitJ) {
+			ParagraphList.leftParagraphList.add(new Paragraph());
+			ParagraphList.rightParagraphList.add(new Paragraph());
+			t++;
+			while(j < limitJ) {
+				ParagraphList.leftParagraphList.get(t).setSeq("\n");
+				ParagraphList.rightParagraphList.get(t).setSeq(right.get(lcs.getRightLCS(i)));
+				j++;
+			}
+		}
+		for(i = 0; i < t; i++) {
+			for(j = 0; j < ParagraphList.leftParagraphList.get(i).getSize(); j++) {
+				System.out.print(ParagraphList.leftParagraphList.get(i).getSeq(j));
 			}
 		}
 	}
