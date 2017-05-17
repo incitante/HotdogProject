d374d0c5a40aff4f87beae060c9a086868dd5ed1
kjlkggg <kjlkg@naver.com>
2017-05-17 19:38:57 +0900

    Add files via upload

--- src/hotdogProject/Merge.java
+++ src/hotdogProject/Merge.java
@@ -1,35 +1,18 @@
-package hotdogProject;
 import javax.swing.JTextPane;
 
 public class Merge {
-	
-	
 
-	static void mergeToRight(JTextPane leftPane,JTextPane rightPane){
-		int start = 0;
-		int end = 0;
-    	  String temp;
-    	  int position=rightPane.getCaretPosition();
-      	  if(position<=end&&start<=position){
-      		  rightPane.select(start,end);
-      		  temp=rightPane.getSelectedText();
-      		  leftPane.select(start,end);
-      		  leftPane.replaceSelection(temp);
-      	  }
-      	
-	}
-	
-	static void mergeToLeft(JTextPane leftPane,JTextPane rightPane){
-		int start = 0;
-		int end = 0;
-  	  String temp;
-  	  int position=rightPane.getCaretPosition();
-    	  if(position<=end&&start<=position){
-    		  rightPane.select(start,end);
-    		  temp=rightPane.getSelectedText();
-    		  leftPane.select(start,end);
-    		  leftPane.replaceSelection(temp);
-    	  }
-    	
+	static void merge(JTextPane currentPane, JTextPane copyPane) {
+		// start : paragraph의 시작 인덱스 end: paragraph의 끝 인덱스
+		int startIdx = 0;
+		int endIdx = 50;
+		int position = currentPane.getCaretPosition();
+
+		if (position <= endIdx && startIdx <= position) {
+			currentPane.select(startIdx, endIdx);
+			String copyText = currentPane.getSelectedText();
+			copyPane.select(startIdx, endIdx);
+			copyPane.replaceSelection(copyText);
+		}
 	}
 }
