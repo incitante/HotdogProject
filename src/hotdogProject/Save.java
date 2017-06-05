d3615bb87d7757823ba8f3f0409ce5ef3896026d
Nangani <nung4567@naver.com>
2017-06-05 18:44:50 +0900

    Add files via upload

--- src/hotdogProject/Save.java
+++ src/hotdogProject/Save.java
@@ -3,8 +3,10 @@ package hotdogProject;
 
 import java.awt.Frame;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JTextPane;
 
@@ -13,14 +15,28 @@ public class Save {
 
 	public Save(JTextPane textPane, String fileName) {
 		fmd.setSize(100, 50);
+		File file = null;
 		try {
+			if(fileName == null) {
+				JFileChooser fc = new JFileChooser();
+				fc.setDialogTitle("Browser for save");
+				
+				int returnVal = fc.showOpenDialog(null);
+				if(returnVal == JFileChooser.APPROVE_OPTION) {
+					file = fc.getSelectedFile();
+				}
+				BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+				textPane.write(writer);
+				JOptionPane.showMessageDialog(fmd, "Save Success");
+				writer.close();
+			} else {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
 			textPane.write(writer);
-			JOptionPane.showMessageDialog(fmd, "저장 성공");
+			JOptionPane.showMessageDialog(fmd, "Save Success");
 			writer.close();
-
+			}
 		} catch (Exception e2) {
-			JOptionPane.showMessageDialog(fmd, "저장 실패");
+			JOptionPane.showMessageDialog(fmd, "Save Failed");
 		}
 	}
 }
